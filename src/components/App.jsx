c8cff51e0c0d718cf9c6b42089b48b522ea09614
ThomasFilipiuk <ThomasFilipiuk2024@u.northwestern.edu>
2021-01-05 22:05:25 -0600

    completed section 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import notes from "../notes";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
 
 function App() {
   return (
     <div>
+      <Header />
+      <Footer />
+      <Note 
+          title = "This is the title"
+          content = "This is the content"
+      />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+var year = new Date().getFullYear();
+var text = "Copyright © " + year;
+
+function Footer() {
+    return (
+        <footer>
+            <p>{text}</p>
+        </footer>
+    )
+}
+
+export default Footer
